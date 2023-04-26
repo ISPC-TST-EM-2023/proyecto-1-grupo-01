a94231de5b87ae03fddb4e825964dfe0be0cd758
gastonloco <ferreyragaston351@gmail.com>
2023-04-26 01:29:42 -0300

    Inicio TC 2 main.cpp 2

--- D_Proyecto/Software/Trabajo Colaborativo 2/Trabajo Colaborativo 2/src/main.cpp
+++ D_Proyecto/Software/Trabajo Colaborativo 2/Trabajo Colaborativo 2/src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-// defino los pines como entradas Analogicas
+
+// defino los pines como entradas Analógicas
+
 #define adc_1 3
 #define adc_2 4
 #define adc_3 5
@@ -13,22 +15,42 @@
 #define dcl_3 17
 #define dcl_4 18
 
-// defino Led de verificacion de entradas Digitales
+// defino Led de verificación de entradas Digitales
 #define Led 2
 
+
+// creo una función para el estado del led
+void encender_led(){
+  digitalWrite(Led,!digitalRead(Led));
+  Serial.println("Pin digital presionado");
+}
 void setup() {
-  // Iniciamos la comunicacion serial a 115200 b/s
+
+  // Iniciamos la comunicación serial a 115200 b/s
+  
   Serial.begin(115200);
+
   // Mensaje por puerto serial
+
   Serial.println("Proceso exitoso !!!");
+
   // Configuramos Led como salida
+
   pinMode(Led,OUTPUT);
 
-  // Configuramos las entradas digitales como interrucciones
+  // Configuramos las entradas digitales como interrupciones
+
   pinMode(dcl_1,INPUT_PULLDOWN);
   pinMode(dcl_2,INPUT_PULLDOWN);
   pinMode(dcl_3,INPUT_PULLDOWN);
   pinMode(dcl_4,INPUT_PULLDOWN);
+
+  //llamo a la función led con cada interrupción
+
+  attachInterrupt(dcl_1,encender_led,FALLING);
+  attachInterrupt(dcl_2,encender_led,FALLING);
+  attachInterrupt(dcl_3,encender_led,FALLING);
+  attachInterrupt(dcl_4,encender_led,FALLING);
 
 }
 
