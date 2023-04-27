d3f4d49c9a3b393f9094804fd62963932385934a
gastonloco <ferreyragaston351@gmail.com>
2023-04-27 02:13:58 -0300

    Inicio TC 2 main.cpp 4

--- D_Proyecto/Software/Trabajo Colaborativo 2/Trabajo Colaborativo 2/src/main.cpp
+++ D_Proyecto/Software/Trabajo Colaborativo 2/Trabajo Colaborativo 2/src/main.cpp
@@ -4,26 +4,63 @@
 
 // defino los pines como entradas Analógicas
 
-#define adc_1 3
+#define adc_1 2
 #define adc_2 4
-#define adc_3 5
-#define adc_4 6
+#define adc_3 13
+#define adc_4 15
 
 // defino los pines como entradas Digitales
-#define dcl_1 15
-#define dcl_2 16
-#define dcl_3 17
-#define dcl_4 18
+#define btn1 34
+#define btn2 35
+#define btn3 32
+#define btn4 33
 
 // defino Led de verificación de entradas Digitales
-#define Led 2
+#define Led_1 25
+#define Led_2 26
+#define Led_3 27
+#define Led_4 14
 
+
+#define DELAY_BTN 600   // 500ms
+
+int i = 0;
+int btnPress = 0;
+long lastTime = 0;
 
 // creo una función para el estado del led
-void encender_led(){
-  digitalWrite(Led,!digitalRead(Led));
-  Serial.println("Pin digital presionado");
+void ISR_1(){
+  if (!digitalRead(btn1)){
+    digitalWrite(Led_1,!digitalRead(Led_1));
+    btnPress = 1;
+  } 
+  else btnPress = 0;
 }
+
+void ISR_2(){
+  if (!digitalRead(btn2)){
+    digitalWrite(Led_2,!digitalRead(Led_2));
+    btnPress = 2;
+  } 
+  else btnPress = 0;
+}
+
+void ISR_3(){
+  if (!digitalRead(btn3)){
+    digitalWrite(Led_3,!digitalRead(Led_3));
+    btnPress = 3;
+  } 
+  else btnPress = 0;
+}
+
+void ISR_4(){
+  if (!digitalRead(btn4)){
+    digitalWrite(Led_4,!digitalRead(Led_4));
+    btnPress = 4;
+  } 
+  else btnPress = 0;
+}
+
 void setup() {
 
   // Iniciamos la comunicación serial a 115200 b/s
@@ -32,21 +69,24 @@ void setup() {
 
   // Configuramos Led como salida
 
-  pinMode(Led,OUTPUT);
+  pinMode(Led_1,OUTPUT);
+  pinMode(Led_2,OUTPUT);
+  pinMode(Led_3,OUTPUT);
+  pinMode(Led_4,OUTPUT);
 
   // Configuramos las entradas digitales como interrupciones
 
-  pinMode(dcl_1,INPUT_PULLDOWN);
-  pinMode(dcl_2,INPUT_PULLDOWN);
-  pinMode(dcl_3,INPUT_PULLDOWN);
-  pinMode(dcl_4,INPUT_PULLDOWN);
+  pinMode(btn1, INPUT_PULLDOWN);       // INPUT_PULLDOWN/INPUT_PULLUP
+  pinMode(btn2, INPUT_PULLDOWN);       // INPUT_PULLDOWN/INPUT_PULLUP
+  pinMode(btn3, INPUT_PULLDOWN);       // INPUT_PULLDOWN/INPUT_PULLUP
+  pinMode(btn4, INPUT_PULLDOWN);       // INPUT_PULLDOWN/INPUT_PULLUP
 
   //llamo a la función led con cada interrupción
 
-  attachInterrupt(dcl_1,encender_led,FALLING);
-  attachInterrupt(dcl_2,encender_led,FALLING);
-  attachInterrupt(dcl_3,encender_led,FALLING);
-  attachInterrupt(dcl_4,encender_led,FALLING);
+  attachInterrupt(btn1, ISR_1, FALLING);  // LOW/HIGH/FALLING/RISING/CHANGE
+  attachInterrupt(btn2, ISR_2, FALLING);  // LOW/HIGH/FALLING/RISING/CHANGE  
+  attachInterrupt(btn3, ISR_3, FALLING);  // LOW/HIGH/FALLING/RISING/CHANGE
+  attachInterrupt(btn4, ISR_4, FALLING);  // LOW/HIGH/FALLING/RISING/CHANGE 
 
   // Mensaje por puerto serial
 
@@ -67,5 +107,21 @@ void loop(){
   Serial.print("ADC_4 Value: ");
   Serial.println(analogRead(adc_4));
 
-  delay(1000);
+  
+
+    if (btnPress)
+  {
+    if (millis()-lastTime > DELAY_BTN)
+    {
+      Serial.print("¡Interrupción ");
+      Serial.print(btnPress);
+      Serial.println("!");
+
+      lastTime = millis();
+    }
+    btnPress = 0;
+  }
+  Serial.println(i);
+  i++;
+delay(600);
 }
